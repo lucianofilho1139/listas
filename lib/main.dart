8e5afa5d50f9c646f42a26a66f8802802442f1d1
lucianofilho1139 <lucianofilho1139@gmail.com>
2021-11-20 19:17:06 -0300

    add list feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:listas/Home.dart';
-import 'package:cloud_firebase/cloud_firestore.dart';
 
-void main() {
+//Necessário adicionar o package firebase_core para utilizar o método Firabase.iniciatilizeApp()
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  //Professor me orientou a inicializar
+  //o FireBase dentro do app antes do RunApp:
+
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(home: Home()));
+  await Firebase.initializeApp();
+
+  //Este Uso eu retalidade,
+  //Mas as nomenclaturas e a instancia mudou:
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  db.collection("usuarios").doc("001").set({"nome": "Fulano"});
+
+  runApp(MaterialApp(
+    home: App(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
